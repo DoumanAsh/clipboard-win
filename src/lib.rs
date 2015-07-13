91a9fff6e6ad0f157e617b2d268ba8a1f0c743f9
DoumanAsh <custparasite@gmx.se>
2015-07-13 22:56:57 +0300

    0.6.0 Initial ClipboardManager

--- src/lib.rs
+++ src/lib.rs
@@ -31,11 +31,80 @@ use winapi::basetsd::SIZE_T;
 use kernel32::{GlobalAlloc, GlobalLock, GlobalUnlock, GetLastError};
 use user32::{GetClipboardSequenceNumber, SetClipboardData, EmptyClipboard, OpenClipboard, GetClipboardData, CloseClipboard};
 
-///GetClipboardSequenceNumber wrapper.
+///Clipboard manager provides a primitive hack for console application to handle updates of
+///clipboard. It uses ```get_clipboard_seq_num``` to determines whatever clipboard is updated or
+///not. Due to that this manager is a bit hacky and not exactly right way to properly work with
+///clipboard. On other hand it is the best and most easy option for console application as a proper
+///window is required to be created to work with clipboard.
+pub struct ClipboardManager {
+    delay_ms: u32,
+    ok_fn: fn(&String) -> (),
+    err_fn: fn(&String) -> (),
+}
+
+impl ClipboardManager {
+    fn default_ok(text: &String) -> () { println!("Clipboard content: {}", &text); }
+    fn default_err(err_text: &String) -> () { println!("Failed to get clipboard. Reason:{}", &err_text); }
+    ///Construct new ClipboardManager with default settings
+    pub fn new() -> ClipboardManager {
+        ClipboardManager {
+            delay_ms: 100,
+            ok_fn: ClipboardManager::default_ok,
+            err_fn: ClipboardManager::default_err,
+        }
+    }
+
+    ///Configure manager's delay between accessing clipboard.
+    pub fn delay(&mut self, delay_ms: u32) -> &mut ClipboardManager {
+        self.delay_ms = delay_ms;
+        self
+    }
+
+    ///Sets callback for successfully retrieved clipboard's text.
+    pub fn ok_callback(&mut self, callback: fn(&String) -> ()) -> &mut ClipboardManager
+     {
+        self.ok_fn = callback;
+        self
+    }
+
+    ///Sets callback for failed retrieval of clipboard's text.
+    ///
+    ///Error description is passed from ```get_clipboard()```
+    pub fn err_callback(&mut self, callback: fn(&String) -> ()) -> &mut ClipboardManager
+     {
+        self.err_fn = callback;
+        self
+    }
+
+    ///Starts manager loop.
+    ///
+    ///It's infinitely running with delay and checking whatever clipboard is updated.
+    ///In case if it is updated callbacks will be called. Depending on whatever clipboard's text
+    ///was retrieved or not right callback will be called.
+    pub fn run(&self) -> () {
+        let mut clip_num: u32 = get_clipboard_seq_num().unwrap_or_else(|| panic!("Lacks sufficient rights to access clipboard(WINSTA_ACCESSCLIPBOARD)"));
+        loop {
+            // It is very unlikely that we would suddenly start to lack access rights.
+            // So let's just skip this iteration. Maybe it is just Windows bug... ^_^
+            let new_num = get_clipboard_seq_num().unwrap_or(0);
+            if new_num != 0 && clip_num != new_num {
+                clip_num = new_num;
+                match get_clipboard() {
+                    Ok(clip_text) => { (self.ok_fn)(&clip_text) },
+                    Err(err_text) => { (self.err_fn)(&err_text) },
+                }
+            println!(">>>");
+            }
+            std::thread::sleep_ms(self.delay_ms);
+        }
+    }
+}
+
+///Wrapper around ```GetClipboardSequenceNumber```.
 ///
 ///Return result:
 ///
-///* ```Some``` Upon successful retrieval of sequence number.
+///* ```Some``` Contains return value of ```GetClipboardSequenceNumber```.
 ///* ```None``` In case if you do not have access. It means that zero is returned by system.
 pub fn get_clipboard_seq_num() -> Option<u32> {
     let result: u32 = unsafe { GetClipboardSequenceNumber() };
